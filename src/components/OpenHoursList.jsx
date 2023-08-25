e5e9383962eb20b6e83b7e5b2a95a215b577a1c2
joroinnoroff <oinojorgen@gmail.com>
2023-08-26 01:28:20 +0200

    final

--- src/components/OpenHoursList.jsx
+++ src/components/OpenHoursList.jsx
@@ -14,7 +14,7 @@ const OpenHoursList = () => {
   return (
     <div className='flex flex-col flex-wrap w-full mt-5'>
       <h5 className='font-semibold'>We're Open from</h5>
-      <ul className='flex flex-wrap flex-col justify-around gap-3 text-center'>
+      <ul className='flex flex-wrap flex-col-reverse justify-around gap-3 text-center'>
         {Object.entries(openingHours).map(([day, hours]) => (
           <li className='border px-2' key={day}>
             {day}: {hours}
--- src/components/Youngster.jsx
+++ src/components/Youngster.jsx
@@ -9,42 +9,40 @@ import { HiOutlineStar } from "react-icons/hi";
 import { FaChild } from "react-icons/fa";
 const Youngster = () => {
   return (
-    <div>
-      <div className='flex items-center justify-center'>
-      <Lottie animationData={animation} style={{ height: '170px', width: '250px' }} />
-        <Lottie animationData={animationData} style={{ height: '210px', width: '250px' }}/>
-
-      </div>
-      <div className='flex flex-col items-center'>
-        <h2 className="text-center text-5xl font-semibold flex flex-col items-center">
-          Young explorers<FaChild />
-        </h2> 
-
-         <div className=''>
-          <h5 className="text-center space-y-5 w-full indent-2">
-          Are you a young person looking to learn more about science? Come on down to our museum, there’s plenty to see and do. You can learn about Newtonian physics from our bumper swing, or why not travel back in time and meet our resident Woolly Mammoth? Our exhibits are designed to be accessible for interested minds, so make sure you come ready to learn and explore.
-          </h5>
-         </div>
+    
+      <section id='Youngsterclub'>
+        <div className='flex items-center justify-center'>
+        <Lottie animationData={animation} style={{ height: '170px', width: '250px' }} />
+          <Lottie animationData={animationData} style={{ height: '210px', width: '250px' }}/>
+        </div>
+        <div className='flex flex-col items-center'>
+          <h2 className="text-center text-5xl font-semibold flex flex-col items-center">
+            Young explorers<FaChild />
+          </h2>
+           <div className=''>
+            <h5 className="text-center space-y-5 w-full indent-2">
+            Are you a young person looking to learn more about science? Come on down to our museum, there’s plenty to see and do. You can learn about Newtonian physics from our bumper swing, or why not travel back in time and meet our resident Woolly Mammoth? Our exhibits are designed to be accessible for interested minds, so make sure you come ready to learn and explore.
+            </h5>
+           </div>
         
-      </div>
-
-      <div className='flex flex-col items-center mt-5 indent-2'>
-        <h2 className="text-center text-5xl font-semibold flex flex-col items-center">
-        YOUNG STARS CLUB<HiOutlineStar />
-        </h2> 
-
-         <div className=''>
-          <h5 className="text-center space-y-5 w-full">
-          We also have the Young Stars club which meets once a week on a Saturday between 10:00 and 13:00 where you’ll get to explore and experiment with our team of experts. During the school holidays we run special holiday clubs where you can join other children your age to go on a journey of discovery. Each holiday we pick a new theme to explore. To find out more about the holiday club and how you can join, send us a message.
-          </h5>
-         </div>
+        </div>
+        <div className='flex flex-col items-center mt-5 indent-2'>
+          <h2 className="text-center text-5xl font-semibold flex flex-col items-center">
+          YOUNG STARS CLUB<HiOutlineStar />
+          </h2>
+           <div className=''>
+            <h5 className="text-center space-y-5 w-full">
+            We also have the Young Stars club which meets once a week on a Saturday between 10:00 and 13:00 where you’ll get to explore and experiment with our team of experts. During the school holidays we run special holiday clubs where you can join other children your age to go on a journey of discovery. Each holiday we pick a new theme to explore. To find out more about the holiday club and how you can join, send us a message.
+            </h5>
+           </div>
         
-      </div>
+        </div>
+      </section>
 
 
 
       
-    </div>
+    
   )
 }
 
